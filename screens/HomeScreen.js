a57687ae7b6e9557cc84b3d660508276131e5829
Abdellah Slimani <85078022+Slimani-Abdellah@users.noreply.github.com>
2022-07-14 22:15:10 +0100

    Add header and searchBar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TextInput } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation} from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -6,7 +6,7 @@ import {
   UserIcon,
   ChevronDownIcon,
   SearchIcon,
-  AdjustemtsIcon,
+  AdjustmentsIcon,
 } from 'react-native-heroicons/outline'
 
 
@@ -19,23 +19,33 @@ export default function HomeScreen() {
     })
   })
   return (
-    <SafeAreaView>
-      <Text className='text-red-500'>
-        {/* Header */}
-        <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+    <SafeAreaView className='bg-white pt-5'>
+      {/* Header */}
+        <View className='flex-row pb-3 items-center mx-4 space-x-2 '>
           <Image 
             source={{
               uri:'https://links.papareact.com/wru'
             }}
             className='h-7 w-7 bg-gray-300 p-4 rounded-full'
           />
-          <View>
+          <View className="flex flex-1">
             <Text className='font-bold text-gray-500 text-xs'>Deliver Now!</Text>
             <Text className='font-bold text-xl'>Curent Location
               <ChevronDownIcon size={20} color="#00ccbb"/></Text>
           </View>
+
+          <UserIcon size={35} color="#00ccbb"/>
         </View>
-      </Text>
+        {/* Search */}
+        <View className='flex-row items-center space-x-2 pb-2 mx-4'>
+          <View className='flex-row space-x-2 flex-1 bg-gray-200 p-3'>
+            <SearchIcon color="gray" size={20}/>
+            <TextInput placeholder='Restaurants and cuisines' 
+            keyboardType='default'
+            />
+          </View>
+          <AdjustmentsIcon color="#00ccbb" />
+        </View>
     </SafeAreaView>
   )
 }
